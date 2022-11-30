762ea4b681da9b36d7bab38768ea511329353773
Katie <katiefaybehrmann@gmail.com>
2022-11-29 19:27:02 -0700

    commit tree 595e85387dfbf83e4b3050d2b029818a766e1335

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 function hasTargetSum(array, target) {
+    const seenNumbers = new Set(); // initialize an empty Set
+    for (const number of array) {
+      const complement = target - number;
+  
+      // .has returns true if the Set includes the complement
+      if (seenNumbers.has(complement)) return true;
+  
+      // .add adds the number to the Set
+      seenNumbers.add(number);
+    }
+    return false;
   // Write your algorithm here
 }
 
